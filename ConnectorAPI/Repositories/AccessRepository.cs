4d9b531f393dfa76e473395f8446c8f8235e339d
Yassine Elmassaoudi <yassine.elmassaoudi@imedia24.de>
2024-01-25 00:22:08 +0100

    disable tracking where unneeded

--- ConnectorAPI/Repositories/AccessRepository.cs
+++ ConnectorAPI/Repositories/AccessRepository.cs
@@ -11,19 +11,25 @@ public class AccessRepository
 {
     private readonly ConnectorDbContext _db;
     private readonly IMemoryCache _memory;
+    private readonly ILogger<AccessRepository> _logger;
+
     private static readonly Func<ConnectorDbContext, string, string, string, string, short, Connection?> GetConnectionQuery =
     EF.CompileQuery(
-        (ConnectorDbContext db, string ownerNode, string guestNode, string resourceId, string resourceName, short accessLevel) => db.Connections
+        (ConnectorDbContext db, string ownerNode, string guestNode, string resourceId, string resourceName, short accessLevel) => db
+                .Connections
                 .Where(cn => cn.OwnerNode == ownerNode && cn.AccessorNode == guestNode)
                 .Include(c => c.Resources.Where(r => r.ResourceId == resourceId && r.ResourceName == resourceName))
                     .ThenInclude(r => r.Attributes.Where(at => accessLevel >= at.MinimumAccessLevel))
+                .AsSingleQuery()
+                .AsNoTracking()
                 .SingleOrDefault()
     );
 
-    public AccessRepository(ConnectorDbContext db, IMemoryCache memory)
+    public AccessRepository(ConnectorDbContext db, IMemoryCache memory, ILogger<AccessRepository> logger)
     {
         _db = db;
         _memory = memory;
+        _logger = logger;
     }
 
     public Connection? GetConnection(AccessRequest access)
@@ -33,12 +39,14 @@ public class AccessRepository
 
     public Connection? GetConnection(string ownerNode, string guestNode, string resourceId, string resourceName, short accessLevel)
     {
-        string key = string.Join("::", ownerNode, guestNode, resourceId, resourceName, accessLevel);
+        string key = string.Join("::", ownerNode, guestNode, resourceName, accessLevel);
 
         return _memory.GetOrCreate(
             key,
             (entry) =>
             {
+                _logger.LogInformation("Cache miss for key {0}", key);
+
                 entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5);
                 return GetConnectionQuery(_db, ownerNode, guestNode, resourceId, resourceName, accessLevel);
             }
